f8bd19184b685acd5ab0617bb8a2b1888bdc1a91
georapbox <georapbox@gmail.com>
2015-12-06 11:26:39 +0200

    Generate css

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -10,7 +10,7 @@ config(['$mdThemingProvider', function ($mdThemingProvider) {
 
     });
 }]).
-controller('AppCtrl', ['$scope', function ($scope) {
+controller('AppCtrl', ['$scope', '$timeout', '$mdDialog', function ($scope, $timeout, $mdDialog) {
     $scope.parent = {
         flexDirection:  'row',
         flexWrap:       'nowrap',
@@ -42,5 +42,62 @@ controller('AppCtrl', ['$scope', function ($scope) {
 
     for (var i = 0; i < 3; i++) {
         addChild();
+    }
+
+    $scope.showDialog = function ($event) {
+        var parentEl = angular.element(document.body);
+        $mdDialog.show({
+            parent: parentEl,
+            targetEvent: $event,
+            clickOutsideToClose: true,
+            template: '' +
+            '<md-dialog flex="50" aria-label="List dialog">' +
+            '    <md-dialog-content>' +
+            '        <pre style="margin:0;"><code style="padding:15px;" class="css">' + generateCSS($scope.parent, $scope.children) + '</code></pre>' +
+            '    </md-dialog-content>' +
+            '</md-dialog>',
+            onShowing: function () {
+                $timeout(function () {
+                    var code = document.querySelectorAll('code');
+                    [].forEach.call(code, function (c) {
+                        hljs.highlightBlock(c);
+                    });
+                }, 0);
+            }
+        });
+    };
+
+    $scope.closeDialog = function () {
+        $mdDialog.cancel();
+    };
+
+    function generateCSS(parent, children) {
+        var parentCSS,
+            childrenCSS;
+
+        parentCSS = '/* Parent CSS */\n' +
+            '.flex-container {\n' +
+            '    ' + 'display: flex;\n' +
+            '    ' + 'align-content: ' + parent.alignContent + ';\n' +
+            '    ' + 'align-items: ' + parent.alignItems + ';\n' +
+            '    ' + 'flex-direction: ' + parent.flexDirection + ';\n' +
+            '    ' + 'flex-wrap: ' + parent.flexWrap + ';\n' +
+            '    ' + 'justify-content: ' + parent.justifyContent + ';\n' +
+            '}\n\n';
+
+        childrenCSS = '/* Children CSS */\n';
+
+        children.forEach(function (child, index) {
+            childrenCSS += '' +
+                '.flex-item:nth-child(' + (++index) + ') {\n' +
+                '    ' + 'order: ' + child.order + ';\n' +
+                '    ' + 'flex-grow: ' + child.flexGrow + ';\n' +
+                '    ' + 'flex-shrink: ' + child.flexShrink + ';\n' +
+                '    ' + 'flex-basis: ' + child.flexBasis + ';\n' +
+                '    ' + 'align-self: ' + child.alignSelf + ';\n' +
+                '}\n\n';
+        });
+
+        return parentCSS + childrenCSS;
     }
 }]);
